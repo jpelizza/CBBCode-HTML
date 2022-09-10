8c1dcc3a5d042e10527400cade2aae69a4d3c3aa
jpmkden <joaopelizza@protonmail.com>
2022-09-10 00:12:15 -0300

    Printing result of function and end buffer

--- main.c
+++ main.c
@@ -4,7 +4,8 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-    char bbcode[1024] = "ASD[b]ASD[code]ASD[/b]asSDAAS[/code]";
-    char *buf;
-    bbcodetohtml(bbcode,*buf,-1);
+	char bbcode[1024] = "ASD[b]ASD[code]ASD[/b]asSDAAS[/code]";
+	char *buf;
+	printf("%d\n", bbcodetohtml_simple(bbcode, &buf, -1));
+	printf("str:%s mem:%d\n\n", buf, (void *)&buf);
 }
