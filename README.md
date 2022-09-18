59e00a1461558b184e58ea8f2376764cd1e95ed8
jpmkden <joaopelizza@protonmail.com>
2022-09-18 17:59:13 -0300

    rewritting to make it more generic

--- README.md
+++ README.md
@@ -1,25 +1,11 @@
-stolen from geeks4geeks
+This project proposes a simples string input string output conversor from bbcode to html
+posteriorly adding support to custom emoticons and tags.
 
-| Metacharacter          | Description     |
-|--------------|-----------|
-| . | To match any character once. The dot character matches a literal dot, within POSIX bracket expressions. For example, a.c matches “abc”, etc., but [a.c] matches only “a”, “.”, or “c”.      |
-| –     | Used to define a range. For Example, [a-d] will match for chars a to d, both inclusive.  |
-| [ ]     | To match anything inside the square brackets. For e.g. [ab] will match a or b.  |
-| ^     | 	The ^ (caret) within the square brackets negates the expressions. For example, [^a] will match anything except a.  |
-|$      | 	The dollar symbol matches the ending position of the string if it is the last character of the regular expression.  |
-|*      | 	To match the preceding character 0 or more times. For example, a*d will match and, annnd, aaaaad, etc.  |
-|{n}      | 	To match the preceding chars n times. Example, [0-9]{3} will match 123, 234, 345, etc.  |
-|{n,m}      | 	To match preceding char at least n times and not more than m times. Example, [0-9]{3,5} will match 123, 3456, 45668, etc.  |
+I'm using https://www.bbcode.org/reference.php as a reference of all tags that should be implemented by default.
 
-Things are starting to shape up.
-As far as I know the simple bbcode is working just fine.
-Next up is complex bbcode, which will need a whole new section of the code, probably to be structured like this:
-bbcodetohtml(...)
+Currently supported:
+\[b] \[i] \[u] \[s] \[color=] \[quote] \[url] \[url=] \[img] \[code]
 
-bbcodetohtml(...){
-    simple(...);
-    complex(...);
-    listsimple(...);
-    listcomplex(...);
-    return (...);
-}
+currenty NOT supported:
+\[size=] \[center] \[left] \[right] \[quote=] \[spoiler] \[spoiler=] \[img width={width} height={height}]
+\[ul] \[li] \[code={language}] \[pre] \[table] \[tr] \[th] \[youtube]
--- cbbhtml.c
+++ cbbhtml.c
@@ -28,28 +28,32 @@ int bbcodetohtml_simple(const char *bbcode, char **buffer, int buffer_size) {
 	// REGEX: \[(b|i|u|s|url|quote|code)\].*?\[\/\1\]
 	// REGEX IMAGE: \[img\](.*?)\[\/img\]
 	// REGEX URL= and COLOR=: \[((url|color)=)(#.+?|https?:\/\/.+?)\](.*)?\[\/\5\]
-	PCRE2_SPTR pattern = "\\[(b|i|u|s|url|quote|code)\\].*?\\[\\/\\1\\]|"				 // 2,3
-						 "\\[(img)\\].*?\\[(\\/img)\\]|"								 // 4,5,6,7
-						 "\\[((url|color)=)(#.+?|https?:\\/\\/.+?)\\](.*?)\\[\\/\\5\\]"; // 8,9,10,11,12,13,14,15
+	PCRE2_SPTR pattern =
+		"\\[(b|i|u|s|center|left|right|quote|spoiler|url|img|code|pre|youtube)\\].*?\\[\\/\\1\\]"; // 2,3
+	//"\\[((url|color)=)(#[0-9A-F]{6}?|https?:\\/\\/.+?)\\](.*?)\\[\\/\\5\\]"; // 8,9,10,11,12,13,14,15
 
-	const char bb_tokens[][32] = {"b", "i", "u", "s", "url", "quote", "code", "img", "url=", "color="};
-	const char html_tokens_begin[][32] = {"<strong>",	  "<em>",  "<ins>",		 "<del>",	   "<a href\"#\">",
-										  "<blockquote>", "<pre>", "<img src='", "<a href=\"", "<div style=\"color:"};
-	const char html_tokens_end[][32] = {"</strong>",	 "</em>",  "</ins>", "</del>", "</a>",
-										"</blockquote>", "</pre>", "'/>",	 "\">",	   "\">"};
+	const char bb_tokens[][16] = {"b", "i", "u", "s", "url", "quote", "code", "img", "url=", "color="};
+	const char html_tokens_begin_open[][16] = {
+		"<strong>",		"<em>",	 "<ins>",	   "<del>",		 "<a href\"#\">",
+		"<blockquote>", "<pre>", "<img src='", "<a href=\"", "<div style=\"color:"};
+	const char html_tokens_begin_close[][16] = {"", "", "", "", "", "", "", "/>", "\">", "\">"};
+	const char html_tokens_end[][16] = {"</strong>",	 "</em>",  "</ins>", "</del>", "</a>",
+										"</blockquote>", "</pre>", "",		 "</a>",   "</div>"};
 
-	if (buffer_size == -1) {
-		*buffer = (char *)malloc((sizeof(char) * strlen(bbcode)) + 1);
-		buffer_size = strlen(bbcode) + 1;
-	} else if (buffer_size < strlen(bbcode)) { // broken
-		*buffer = (char *)realloc(*buffer, (sizeof(char) * strlen(bbcode)) + 1);
-		buffer_size = strlen(bbcode) + 1;
+	// MALLOC BUFFER
+	{
+		if (buffer_size == -1) {
+			*buffer = (char *)malloc((sizeof(char) * strlen(bbcode)) + 1);
+			buffer_size = strlen(bbcode) + 1;
+		} else if (buffer_size < strlen(bbcode)) { // broken
+			*buffer = (char *)realloc(*buffer, (sizeof(char) * strlen(bbcode)) + 1);
+			buffer_size = strlen(bbcode) + 1;
+		}
+		if (*buffer == NULL || regex == NULL)
+			return 1;
+		strcpy(*buffer, bbcode);
 	}
-	if (*buffer == NULL || regex == NULL)
-		return 1;
-
-	strcpy(*buffer, bbcode);
-
+	// REGEX COMPILE
 	regex = pcre2_compile(pattern, -1, 0, &errorcode, &erroffset, NULL);
 	if (regex == NULL) {
 		return 2;
@@ -58,84 +62,40 @@ int bbcodetohtml_simple(const char *bbcode, char **buffer, int buffer_size) {
 	// Search for all instances of regex from left to right
 	while (pcre2_match(regex, *buffer, -1, 0, 0, matches, NULL) > 0) {
 		m = pcre2_get_ovector_pointer(matches);
-		for (int i = 0; i < 16; i++) {
-			printf("[%d] - %d\n", i, m[i]);
-			printf("%s\n", *buffer + m[i]);
-		}
 
-		int symbol;
+		int symbol, sp;
 		char *subgroup;
-		for (int i = 2; i < 10; i += 2) {
-			if (m[i] != -1) {
-				subgroup = (char *)malloc(sizeof(char) * (m[i + 1] - m[i] + 1));
-				memset(subgroup, '\0', m[i + 1] - m[i] + 1);
-				strncpy(subgroup, *buffer + m[i], m[i + 1] - m[i]);
+
+		for (sp = 2; sp < 10; sp += 2) {
+			if (m[sp] != -1) {
+				subgroup = (char *)malloc(sizeof(char) * (m[sp + 1] - m[sp] + 1));
+				memset(subgroup, '\0', m[sp + 1] - m[sp] + 1);
+				strncpy(subgroup, *buffer + m[sp], m[sp + 1] - m[sp]);
+				symbol = (sp / 2) - 1;
 				break;
 			}
 		}
-		printf("sub: %s\n", subgroup);
 
-		for (symbol = 0; symbol < 7; symbol++) {
-			int bbt_len = strlen(bb_tokens[symbol]);
-			// REPLACES FOR: "b", "i", "u", "s", "url", "quote", "code"
-			// 2,3
-			if (!strcmp(subgroup, bb_tokens[symbol])) {
-				printf("::1::\n");
-				str_replace(buffer, &buffer_size, *buffer + m[1] - (bbt_len + 3), bbt_len + 3, html_tokens_end[symbol]);
-				str_replace(buffer, &buffer_size, *buffer + m[0], bbt_len + 2, html_tokens_begin[symbol]);
-				break;
-			}
-		}
-		for (symbol = 7; symbol < 8; symbol++) {
-			int bbt_len = strlen(bb_tokens[symbol]);
-			// REPLACES FOR: "img"
-			if (!strcmp(subgroup, bb_tokens[symbol])) {
-				printf("::2::\n");
-				int tmp_replacer_size = (m[1] - m[0]) + (strlen(html_tokens_begin[7]) + strlen(html_tokens_end[7]));
-				char *tmp_replacer = (char *)malloc(sizeof(char) * (tmp_replacer_size));
-				memset(tmp_replacer, '\0', tmp_replacer_size);
-				strcpy(tmp_replacer, html_tokens_begin[7]);
-				strncat(tmp_replacer, *buffer + m[4] + 4, (m[7] - 6) - m[5]);
-				strcat(tmp_replacer, html_tokens_end[7]);
-				str_replace(buffer, &buffer_size, *buffer + m[1] - (bbt_len + 3), bbt_len + 3, "");
-				str_replace(buffer, &buffer_size, *buffer + m[0], (bbt_len + 2) + ((m[7] - 6) - m[5]), tmp_replacer);
-				free(tmp_replacer);
-				continue;
-			}
-		}
-		// REPLACE FOR "URL=" and "color="
-		//[5] 8,9,
-		//[6]10,11,
-		//[7]12,13
-		//[8]14,15
-		// REGEX URL= and COLOR=: \[((url|color)=)(#.+?|https?:\/\/.+?)\](.*)?\[\/\5\]
-		//						   8|10       11|9|12                13 |14|15
-		if (!strcmp(subgroup, bb_tokens[8])) {
-			int symbol = 8;
-			int tmp_replacer_size = (8 + (m[13] - m[12]) + (m[15] - m[14]));
-			char *tmp_replacer = (char *)malloc(sizeof(char) * tmp_replacer_size);
-			memset(tmp_replacer, '\0', tmp_replacer_size);
+		size_t OPEN_SIZE = strlen(html_tokens_begin_open[symbol]);
+		size_t CLOSE_SIZE = strlen(html_tokens_begin_close[symbol]);
+		size_t END_SIZE = strlen(html_tokens_end[symbol]);
+		size_t TAG_SIZE = strlen(bb_tokens[symbol]);
+		// '5' comes from '[]'+'[/]' that are always present
+		// 2*TAG since tag both opens and closes
+		signed long REPLACE_SIZE = OPEN_SIZE + CLOSE_SIZE + END_SIZE - (5 + (2 * TAG_SIZE));
+		// tmp_replacer_size = len(group_1)+REPLACE_SIZE
+		int tmp_replacer_size = (m[1] - m[0]) + REPLACE_SIZE;
+		char *tmp_replacer = (char *)malloc(sizeof(char) * (tmp_replacer_size));
 
-			strcpy(tmp_replacer, html_tokens_begin[symbol]);
-			strncat(tmp_replacer, *buffer + m[12], (m[13] - m[12]));
-			strcat(tmp_replacer, html_tokens_end[symbol]);
+		memset(tmp_replacer, '\0', tmp_replacer_size);
+		strcpy(tmp_replacer, html_tokens_begin_open[7]);
+		strncat(tmp_replacer, *buffer + m[4] + 4, (m[7] - 6) - m[5]);
+		strcat(tmp_replacer, html_tokens_begin_close[7]);
 
-			str_replace(buffer, &buffer_size, *buffer + m[1] - 6, 6, "</a>");
-			str_replace(buffer, &buffer_size, *buffer + m[0], (m[13] - m[12]) + 6, tmp_replacer);
-			free(tmp_replacer);
-		} else if (!strcmp(subgroup, bb_tokens[9])) {
-			int symbol = 9;
-			int tmp_replacer_size = (8 + (m[13] - m[12]) + (m[15] - m[14]));
-			char *tmp_replacer = (char *)malloc(sizeof(char) * tmp_replacer_size);
-			memset(tmp_replacer, '\0', tmp_replacer_size);
+		str_replace(buffer, &buffer_size, *buffer + m[1] - (TAG_SIZE + OPEN_SIZE + CLOSE_SIZE), TAG_SIZE + 3, "");
+		str_replace(buffer, &buffer_size, *buffer + m[0], (TAG_SIZE + 2) + ((m[7] - 6) - m[5]), tmp_replacer);
 
-			strcpy(tmp_replacer, html_tokens_begin[symbol]);
-			strncat(tmp_replacer, *buffer + m[12], (m[13] - m[12]));
-			strcat(tmp_replacer, html_tokens_end[symbol]);
-			str_replace(buffer, &buffer_size, *buffer + m[1] - 8, 8, "</div>");
-			str_replace(buffer, &buffer_size, *buffer + m[0], (m[13] - m[12]) + 8, tmp_replacer);
-			free(tmp_replacer);
-		}
+		free(tmp_replacer);
 		free(subgroup);
 	}
 	pcre2_match_data_free(matches);
